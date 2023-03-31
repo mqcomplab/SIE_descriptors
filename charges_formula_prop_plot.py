b94469e97e03f2a2b4b874c5b918915cfee45439
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-03-31 16:40:37 -0400

    Update charges_formula_prop_plot.py

--- charges_formula_prop_plot.py
+++ charges_formula_prop_plot.py
@@ -1,4 +1,7 @@
-# This script generates multiple plots for fitting anions/cations using all 16 formulas and all 9 properties.
+"""
+This script generates multiple plots for fitting anions/cations using all 16 formulas and all 9 properties.
+Author: Lexin Chen
+"""
 import modules as mod
 import matplotlib.pyplot as plt
 import time
