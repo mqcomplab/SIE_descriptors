b29e0e7123481f08769756cb32fa93cacc616bf7
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-04-15 21:55:10 -0400

    updated

--- charges_formula_prop_plot.py
+++ charges_formula_prop_plot.py
@@ -8,13 +8,13 @@ import time
 from contextlib import suppress
 import os
 
-start = time.perf_counter()
 ion_type = "cations"    # edit: anions or cations
 file_name = f"csv/{ion_type}.csv"
 formula_list = ["Q1R1", "Q1R2", "Q2R1", "Q2R2", "Q3R1","Q3R2", "Q4R1", "Q4R2", "Q5R1", "Q5R2", "Q6R1","Q6R2", "Q7R1", "Q7R2", "Q8R1", "Q8R2"]
 property_list = ["dG", "dH", "viscosity", "diffusion", "gfet", "dlcst", "sn2", "activity", "lysozyme"]
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     for per, property in enumerate(property_list):
         for each, formula in enumerate(formula_list):
             formula_func = getattr(mod.formulas, formula)
@@ -34,5 +34,5 @@ if __name__ == "__main__":
             plt.close()
             print(f"Finished with {formula}: {property}..")
 
-end = time.perf_counter()
-print(f'Finished in {round(end-start,2)} seconds')      # Anions take 8.9 min and cations take 1.6 min on a regular laptop.
+    end = time.perf_counter()
+    print(f'Finished in {round(end-start,2)} seconds')      # Anions take 8.9 min and cations take 1.6 min on a regular laptop.
